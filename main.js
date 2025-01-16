f85a7be815d758f37bcc2e09d1b3eaff594f881d
谢残 <chloe@fulanyingwu-3.local>
2025-01-15 19:45:53 -0600

    refine main.js

--- main.js
+++ main.js
@@ -16,6 +16,24 @@ if (photosPage) {
         "assets/images/photo1.JPG",
         "assets/images/photo2.JPG",
         "assets/images/photo3.JPG",
+        "assets/images/photo4.JPG",
+        "assets/images/photo5.JPG",
+        "assets/images/photo6.JPG",
+        "assets/images/photo7.JPG",
+        "assets/images/photo8.JPG",
+        "assets/images/photo9.JPG",
+        "assets/images/photo10.JPG",
+        "assets/images/photo11.JPG",
+        "assets/images/photo12.JPG",
+        "assets/images/photo13.JPG",
+        "assets/images/photo14.JPG",
+        "assets/images/photo15.JPG",
+        "assets/images/photo16.JPG",
+        "assets/images/photo17.JPG",
+        "assets/images/photo18.JPG",
+        "assets/images/photo19.JPG",
+        "assets/images/photo20.JPG",
+        
         // Add paths to all your photos here
     ];
 
